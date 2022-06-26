e58a46b0435a3126f39e5e51cde394dd2e15d538
Rostyslav Kovalchuk <rostykwave@GMAIL.COM>
2022-06-26 21:48:44 +0300

    API started connecting

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,31 @@
-import { Component } from "react";
-import ImageGallery from "./ImageGallery";
-import Modal from "./Modal";
-import Searchbar from "./Searchbar";
-// import { ReactComponent as SearchIcon } from '../icons/search.svg';
+import { Component } from 'react';
+import ImageGallery from './ImageGallery';
+import Modal from './Modal';
+import Searchbar from './Searchbar';
+import { fetchArticlesWithQuery } from 'services/api';
 
-class App extends Component{
-
+class App extends Component {
   state = {
+    articles: [],
+    isLoading: false,
+    error: null,
     showModal: false,
   };
+
+  async componentDidMount() {
+    console.log('componentDidMount');
+    this.setState({ isLoading: true });
+
+    try {
+      const articles = fetchArticlesWithQuery('cat');
+      this.setState({ articles });
+    } catch (error) {
+      console.log('eroorr');
+      this.setState({ error });
+    } finally {
+      this.setState({ isLoading: false });
+    }
+  }
 
   toggleModal = () => {
     this.setState(({ showModal }) => ({
@@ -16,32 +33,31 @@ class App extends Component{
     }));
   };
 
-
   searchQuery = data => {
     console.log(data);
   };
-  
-  render() {
 
+  render() {
     const { showModal } = this.state;
 
     return (
       <>
         <Searchbar onSubmit={this.searchQuery} />
-        <ImageGallery/>
-        <button type="button" onClick={this.toggleModal}>Open modal</button>
+        <ImageGallery />
+        <button type="button" onClick={this.toggleModal}>
+          Open modal
+        </button>
 
         {showModal && (
           <Modal onClose={this.toggleModal}>
-          {/* <img src="" alt="" /> */}
+            {/* <img src="" alt="" /> */}
             {/* <button type="button" onClick={this.toggleModal}>Close</button> */}
             <p>hello</p>
           </Modal>
         )}
-      
       </>
-    )
-  };
+    );
+  }
 }
 
 export default App;
---
+++ src/services/api.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+const KEY = '27289011-631f37c1ff3a5cbdb3c134909';
+const url = `https://pixabay.com/api/?q=cat&page=1&key=${KEY}&image_type=photo&orientation=horizontal&per_page=12`;
+
+export const fetchArticlesWithQuery = async searchQuery => {
+  const response = axios
+    .get(
+      `https://pixabay.com/api/?q=${searchQuery}&page=1&key=${KEY}&image_type=photo&orientation=horizontal&per_page=12`
+    )
+    .then(({ data }) => {
+      console.log(data);
+      return data.hits;
+    });
+};
